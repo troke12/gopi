3d07a864847432918f35261b22d97b5050ca4215
troke12 <jame_glove@yahoo.com>
2021-11-09 07:22:23 +0800

    add config for return the ip behind proxy

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/gofiber/fiber/v2 v2.20.1
-	github.com/oschwald/geoip2-golang v1.5.0 // indirect
+	github.com/oschwald/geoip2-golang v1.5.0
 	github.com/oschwald/maxminddb-golang v1.8.0 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/oschwald/geoip2-golang"
@@ -21,29 +22,64 @@ type IPstruct struct {
 	Postal        string
 }
 
+//Add Test
+
+func getUserIP(c *fiber.Ctx) error {
+   	var userIP string
+   	if len(c.GetRespHeader("CF-Connecting-IP")) > 1 {
+        	userIP = c.GetRespHeader("CF-Connecting-IP")
+        	fmt.Println(net.ParseIP(userIP))
+    	} else if len(c.GetRespHeader("X-Forwarded-For")) > 1 {
+        	userIP = c.GetRespHeader("X-Forwarded-For")
+        	fmt.Println(net.ParseIP(userIP))
+    	} else if len(c.GetRespHeader("X-Real-IP")) > 1 {
+        	userIP = c.GetRespHeader("X-Real-IP")
+        	fmt.Println(net.ParseIP(userIP))
+    	} else {
+        	userIP = c.IP()
+        	if strings.Contains(userIP, ":") {
+            		fmt.Println(net.ParseIP(strings.Split(userIP, ":")[0]))
+        	} else {
+            		fmt.Println(net.ParseIP(userIP))
+        	}
+ 	}
+	return c.JSON(fiber.Map{
+		"ip": net.ParseIP(userIP),
+	})
+}
+
 func main() {
-	app := fiber.New()
+	//Add config for connecting to CF IP
+	app := fiber.New(fiber.Config{
+		ProxyHeader: "CF-Connecting-IP",
+	})
 	//Get the DB
 	db, err := geoip2.Open("data/GeoIP2-City.mmdb")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
-
 	// Routes
+	app.Get("/jancok", getUserIP)
+	app.Get("/test", func(c *fiber.Ctx) error {
+		h := c.IP()
+		fmt.Println(h)
+		return c.JSON(fiber.Map{
+			"ip": h,
+		})
+	})
 	app.Get("/", func(c *fiber.Ctx) error {
-		getClientIP := c.IP()
-		ip := net.ParseIP(getClientIP)
+		getclientIP := c.IP()
+		ip := net.ParseIP(getclientIP)
 		record, err := db.City(ip)
 		if err != nil {
-			//log.Fatalf("error: %v", err)
+			fmt.Printf("error: %v", err)
 			return c.JSON(fiber.Map{
 				"status": "err",
 			})
 		}
 		//buat test di console
 		//fmt.Printf("%v", record)
-
 		dataIP := IPstruct{
 			IP:            fmt.Sprintf("%v", ip),
 			City:          record.City.Names["en"],
@@ -93,5 +129,5 @@ func main() {
 		return c.JSON(dataIP)
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	log.Fatal(app.Listen(":3045"))
 }
